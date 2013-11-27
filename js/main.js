ce8e486bc34048de5662388e5106bdc07bb521bd
Daniel Tse <dan@signia.ca>
2013-11-26 23:49:19 -0700

    working settings for switching flows, and changing stagenames

--- js/main.js
+++ js/main.js
@@ -8,13 +8,38 @@ var init = function () {
         if(e.keyName == "back")
             tizen.application.getCurrentApplication().exit();
     });
+    
+    console.log("window height:"+ window.innerHeight + " width:" + window.innerWidth);
 };
 // window.onload can work without <body onload="">
 window.onload = init;
 
 var app = angular.module('flowapp', ['ngRoute']);
 
+app.factory('dataServices', function(){
+	var myflows = new Object();	
+	
+	return {
+		load: function()
+		{
+			console.log("loading");
+		},
+		save: function()
+		{
+			console.log("saving...");
+		},
+		getFlow: function(id)
+		{
+			
+		},
+		getFlows: function()
+		{
+			return myflows;
+		}
+	}
+});
 
+//Routes
 app.config(['$routeProvider', function($routeProvider) {
 	  $routeProvider.when('/stages', {templateUrl: 'partials/stages.html', controller: 'stagesCtrl'});
 	  $routeProvider.when('/stageview/:stageid', {templateUrl: 'partials/stageview.html', controller: 'stageViewCtrl'});
@@ -23,18 +48,105 @@ app.config(['$routeProvider', function($routeProvider) {
 	  $routeProvider.otherwise({redirectTo: '/stages'});
 	}]);
 
-function stagesCtrl($scope) {
+function stagesCtrl($scope, $location) {
+	$scope.pipeline = {"pipeline1":{
+	    "stages":[{"stageId":"1", "stageName":"Lead", "stageColour":"yellow"}, {"stageName":"Contacted", "stageColour":"orange"}, {"stageName":"Pitched", "stageColour":"red"}, {"stageName":"Sold", "stageColour":"green"}],
+	        "fields":[{"fieldName":"a", "fieldType":"text", "display":"yes/no"}, 
+	                    {"fieldName":"b", "fieldType":"number", "display":"yes/no"}
+	                    ,{"fieldName":"c", "fieldType":"text", "display":"yes/no"}
+	                    ,{"fieldName":"d", "fieldType":"number", "display":"yes/no"}],
+	        "crm":[{"contactid":"1", "field1":"val1", "field2":"val2", "stageId":"stage1"},{"contactid":"2", "field1":"val1", "field2":"val2", "stageId":"stage1"}]}};
+	
+	$scope.stages = $scope.pipeline.pipeline1.stages;
+	
+	$scope.stageBoxHeight = (window.innerHeight - 100) / $scope.stages.length;
+	
+	$scope.viewStage = function(stageid)
+	{
+		$location.path('/stageview/' + stageid);
+	}
 	
 }
 
-function stageViewCtrl($scope) {
+function stageViewCtrl($scope, $location) {
 	
 }
 
-function personViewCtrl($scope) {
+function personViewCtrl($scope, $location) {
 	
 }
 
-function settingsCtrl($scope) {
+function settingsCtrl($scope, $location) {
+	$scope.pipelines = [{"name":"Sales", "id":"1", "pipeline":{
+	    "stages":[{"stageId":"1", "stageName":"Lead", "stageColour":"yellow"}, {"stageName":"Contacted", "stageColour":"orange"}, {"stageName":"Pitched", "stageColour":"red"}, {"stageName":"Sold", "stageColour":"green"}],
+	        "fields":[{"fieldName":"a", "fieldType":"text", "display":"yes/no"}, 
+	                    {"fieldName":"b", "fieldType":"number", "display":"yes/no"}
+	                    ,{"fieldName":"c", "fieldType":"text", "display":"yes/no"}
+	                    ,{"fieldName":"d", "fieldType":"number", "display":"yes/no"}],
+	        "crm":[{"contactid":"1", "field1":"val1", "field2":"val2", "stageId":"stage1"},{"contactid":"2", "field1":"val1", "field2":"val2", "stageId":"stage1"}]}}
+	,{"name":"Ideas", "id":"2", "pipeline":{
+	    "stages":[{"stageId":"1", "stageName":"Lead", "stageColour":"yellow"}, {"stageName":"Contacted", "stageColour":"orange"}, {"stageName":"Pitched", "stageColour":"red"}, {"stageName":"Sold", "stageColour":"green"}],
+        "fields":[{"fieldName":"a", "fieldType":"text", "display":"yes/no"}, 
+                    {"fieldName":"b", "fieldType":"number", "display":"yes/no"}
+                    ,{"fieldName":"c", "fieldType":"text", "display":"yes/no"}
+                    ,{"fieldName":"d", "fieldType":"number", "display":"yes/no"}],
+        "crm":[{"contactid":"1", "field1":"val1", "field2":"val2", "stageId":"stage1"},{"contactid":"2", "field1":"val1", "field2":"val2", "stageId":"stage1"}]}}];
 	
+	$scope.currentPipeline = "";
+	
+	$scope.selectedPipeline; //TODO Add a watch on this?
+	
+	//DOC: If nothing is selected, select the first one.
+	if ($scope.selectedPipeline == null)
+	{
+		$scope.selectedPipeline = $scope.pipelines[0];
+		$scope.currentPipeline = $scope.selectedPipeline;
+	}
+	
+	$scope.switchPipeline = function()
+	{
+		if (angular.equals($scope.currentPipeline, $scope.selectedPipeline) == false)
+		{
+			$scope.currentPipeline = $scope.selectedPipeline;
+			console.log("Changed Pipeline!");
+		}
+	}
+	
+	$scope.changeFlowName = function(newName)
+	{
+		if ( newName.trim().length > 0)
+		{
+			$scope.currentPipeline.name = newName;
+		}
+	}
+	
+	$scope.changeOrder = function(stage, direction)
+	{
+		//Find the index
+		var sourceIndex = $scope.currentPipeline.pipeline.stages.indexOf(stage);
+		var maxIndex = $scope.currentPipeline.pipeline.stages.length - 1;
+
+		if ((direction == -1) && (sourceIndex != 0))
+		{
+			var temp = $scope.currentPipeline.pipeline.stages[sourceIndex - 1];
+			$scope.currentPipeline.pipeline.stages[sourceIndex - 1] = stage;
+			$scope.currentPipeline.pipeline.stages[sourceIndex] = temp;
+		}
+		
+		else if ((direction == 1) && (sourceIndex != maxIndex))
+		{
+			var temp = $scope.currentPipeline.pipeline.stages[sourceIndex + 1];
+			$scope.currentPipeline.pipeline.stages[sourceIndex + 1] = stage;
+			$scope.currentPipeline.pipeline.stages[sourceIndex] = temp;
+		}
+	}
+	
+	$scope.deleteStage = function(stage)
+	{
+		var targetIndex = $scope.currentPipeline.pipeline.stages.indexOf(stage);
+		if (targetIndex != -1)
+		{
+			$scope.currentPipeline.pipeline.stages.splice(targetIndex, 1);
+		}
+	}
 }
